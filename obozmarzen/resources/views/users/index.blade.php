7f0b760f546a31584cd548f7d5e5d77714883a27
kadzus1 <knowicka@interia.pl>
2022-05-25 08:47:33 +0200

    logowanie, rejestracja, połączenie z bazą, nazwa domeny, lista użytkowników i jej stronnicowanie

---
+++ obozmarzen/resources/views/users/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Email!!!</th>
+            <th scope="col">Imię</th>
+            <th scope="col">Nazwisko</th>
+            <th scope="col">Numer telefonu</th>
+            <th scope="col">Akcje</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <th scope="row">{{ $user->id }}</th>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->surname }}</td>
+                <td>{{ $user->phone_number }}</td>
+                <td>
+                    <button class="btn btn-danger btn-sm delete" data-id="{{$user->id}}">
+                        X
+                    </button>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{$users->links()}}
+</div>
+@endsection
+@section('javascript')
+    $(function() {
+    $('.delete').click(function(){
+            $.ajax({
+            method: "DELETE",
+            url: "http://obozmarzen.test/users/" + $(this).data("id")
+          })
+            .done(function( response ) {
+              alert( "SUKCES");
+            })
+            .fail(function (response){
+                alert( "error");
+                    });
+                
+        });
+    });
+
+
+@endsection
